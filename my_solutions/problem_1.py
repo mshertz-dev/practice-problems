07a140b99e70e6bfa04242a4dae7cab00dd83b2b
Mike Shertz <shertzm2@gmail.com>
2020-11-16 16:14:40 -0500

    first python problem for Tyler Lockett stats complete

---
+++ my_solutions/problem_1.py
@@ -0,0 +1,10 @@
+receptions = 69
+targets = 82
+rec_yds = 1057
+rec_tds = 8
+
+catch_rate = receptions / targets
+print('Tyler Lockett\'s catch rate is', catch_rate)
+
+fantasy_pts = receptions + rec_yds * .1 + rec_tds * 6
+print('Tyler Lockett\'s overall fantasy points =', fantasy_pts)
