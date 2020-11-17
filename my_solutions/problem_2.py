9041277ddc13a490e4c106403a86804baf5870ee
Mike Shertz <shertzm2@gmail.com>
2020-11-17 11:28:01 -0500

    finished prompt 2, calculating Michael Thomas' average receiving yards in 2019

---
+++ my_solutions/problem_2.py
@@ -0,0 +1,5 @@
+rec_yds = [123, 89, 54, 95, 182, 89, 131, 112, 152, 114, 101, 48, 134, 128, 136, 37]
+
+total_rec_yds = sum(rec_yds)
+avg_rec_yds = total_rec_yds / len(rec_yds)
+print("Michael Thomas\' average receiving yards in 2019 per game was", avg_rec_yds)
